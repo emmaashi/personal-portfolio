5d8d3c6d9ad5382015e54dc1fa5ee87d496c59bc
emmaashi <emmashi2006@gmail.com>
2024-08-19 23:23:50 -0400

    link

--- data/index.ts
+++ data/index.ts
@@ -72,7 +72,7 @@ export const projects = [
     des: "Removing the bias from media, ensuring that young readers have access to holistic, reliable information regarding current world issues.",
     img: "/newsflix.png",
     iconLists: ["/re.svg", "/py.svg", "/tail.svg", "/postman.svg"],
-    link: "https://github.com/emmaashi/crime-map-canada",
+    link: "https://devpost.com/software/newsflix",
   },
 
   {

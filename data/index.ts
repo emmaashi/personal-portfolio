265e1d091231e16c8e9ad3ba994d136ce8b6de1d
emmaashi <emmashi2006@gmail.com>
2024-11-28 02:25:10 -0500

    update

--- data/index.ts
+++ data/index.ts
@@ -10,14 +10,14 @@ export const articles = [
   {
     id: 1,
     title: "University of Waterloo",
-    description: "Discover the experiences that led me to UW and ignited my passion for innovation and social impact.",
+    description: "Read about the experiences that led me to the University of Waterloo's Computer Science program.",
     img: "/uw.webp",
     link: "https://uwaterloo.ca/news/meet-waterloos-2024-schulich-leaders",
   },
   {
     id: 2,
     title: "Schulich Leader",
-    description: "Selected as 1 of 100 recipients of Canada's largest undergraduate STEM scholarship ($100,000).",
+    description: "Selected as 1 of 100 recipients of Canada's largest undergraduate STEM scholarship.",
     img: "/schulich.png",
     link: "https://schulichleaders.com/scholars/emmashi/",
   },
@@ -33,6 +33,14 @@ export const articles = [
 export const projects = [
   {
     id: 1,
+    title: "compile.",
+    des: "Predictive model trained to analyze historical data and forecast cancer progression. Built using Python, Flask, React, SEER API, Pandas, Plotly, and Javascript.",
+    img: "/compile.png",
+    iconLists: ["/py.svg", "/re.svg", "/js.svg"],
+    date: "November 2024"
+  },
+  {
+    id: 2,
     title: "DAG",
     des: "Using Next.js, MongoDB, Cohere, Tailwind CSS, Flow, React, Syro, and Solana, my team and I created DAG, a one-of-a-kind platform that allows for decentralized data aggregation across all of machine learning. This project won 'Best use of Solana' and 'Best use of Flow' at Hack the North 2023, North America's largest hackathon.",
     img: "/dag.png",
@@ -41,7 +49,7 @@ export const projects = [
     date: "Hack the North 2023"
   },
   {
-    id: 2,
+    id: 3,
     title: "Optimed",
     des: "Aimed at mitigating patient wait times and extending healthcare support to marginalized communities in remote areas.",
     img: "/optimed.png",
@@ -50,7 +58,7 @@ export const projects = [
     date: "Shad Canada 2022"
   },
   {
-    id: 3,
+    id: 4,
     title: "Nightlight",
     des: "Addressing and ensuring the safety of women+ when taking public transport at night. The inspiration behind ‘nightlight’ stemmed from the fear of taking public transportation systems, like the TTC, alone.",
     img: "/nightlight.png",
@@ -59,7 +67,7 @@ export const projects = [
     date: "Technova 2022"
   },
   {
-    id: 4,
+    id: 5,
     title: "FireCAST",
     des: "FIREcast leverages AI to forecast fire risks in geographical regions. The purpose of the tool is to empower communities with actionable insights to allocate resources and implement proactive strategies for wildfire prevention.",
     img: "/firecast.png",
@@ -69,7 +77,7 @@ export const projects = [
   },
 
   {
-    id: 5,
+    id: 6,
     title: "Amazing Labyrinth",
     des: "Strategic maze game where players navigate through shifting pathways to find treasures. This version includes features like shortest path and animations to enhance the gaming experience.",
     img: "/labyrinth.png",
@@ -79,7 +87,7 @@ export const projects = [
   },
 
   {
-    id: 6,
+    id: 7,
     title: "Settlemize",
     des: "Settlemize remains steadfast to optimizing the settlement process for immigrants coming to Canada through the abundance of user-friendly tools that our application has to offer!",
     img: "/settlemize.png",
@@ -89,7 +97,7 @@ export const projects = [
   },
 
   {
-    id: 7,
+    id: 8,
     title: "Divvy",
     des: "Divvy is a project management tool designed to streamline the process of distributing tasks within a group or team, particularly catering to school group projects. The tool provides features for task management, user performance tracking, and project planning.",
     img: "/divvy.png",
@@ -98,7 +106,7 @@ export const projects = [
     date: "June 2024"
   },
   {
-    id: 8,
+    id: 9,
     title: "Newsflix",
     des: "Removing the bias from media, ensuring that young readers have access to holistic, reliable information regarding current world issues.",
     img: "/Newsflix.png",
@@ -108,7 +116,7 @@ export const projects = [
   },
 
   {
-    id: 9,
+    id: 10,
     title: "Crime Map Canada",
     des: "Crime Map leverages data mining from the Ontario provincial website to evaluate crime severity across various cities and provinces in Canada.",
     img: "/crime.png",
@@ -117,7 +125,7 @@ export const projects = [
     date: "April 2024"
   },
   {
-    id: 10,
+    id: 11,
     title: "Traura.Ai",
     des: "Combining speech recognition with semantic similarity models, Traura.ai gamifies your everyday conversations to produce your 'aura' score.",
     img: "/traura.png",
@@ -127,34 +135,33 @@ export const projects = [
   },
 ];
 
-export const workExperience = [
+export const workExperience = [ 
+  {  id: 1,
+    title: "ML/Backend Engineer @ Wat Street",
+    desc: "Developing infrastructure to support the scaling of ML models in quantitative finance applications.",
+    className: "md:col-span-2", 
+    thumbnail: "/watstreet.jpeg",
+  },
   {
-    id: 1,
+    id: 2,
     title: "Software Engineer Intern @ Ply Health (YC S24)",
     desc: "Helping practices get in network with payers. Worked with TypeScript, Supabase, React, Next.js, Vercel, and Git during the development process.",
     className: "md:col-span-2",
     thumbnail: "/ply.jpeg",
   },
   {
-    id: 2,
+    id: 3,
     title: "Program Assistant @ Shad Canada",
     desc: "Coordinated activities, supported program operations, and mentored 120 students at the Ontario Tech University campus.",
     className: "md:col-span-2", 
     thumbnail: "/shad.png",
   },
   {
-    id: 3,
+    id: 4,
     title: "Assistant Teacher @ Spirit of Math",
     desc: "Enhanced the learning experience of students in grade 1, 2, 5, 6, 9, and 10 by aiding in the delivery of advanced math curriculum.",
     className: "md:col-span-2",
     thumbnail: "/som.png",
-  },
-  {
-    id: 4,
-    title: "Finance Executive @ Ignition Hacks",
-    desc: "Collaborated with fellow executives to raise $15,000+ in sponsorships from leading companies in the tech and finance industries.",
-    className: "md:col-span-2",
-    thumbnail: "/ignition.png",
   },
 ];
 
